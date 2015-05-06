e2f0e0a3f9121bb11221367f8782b24bf9af2ad0
sheez <blaat2k@mail.com>
2015-05-06 16:36:40 +0200

    add new contact form + styles

--- footer.php
+++ footer.php
@@ -37,5 +37,6 @@
     </footer> 
 
   <?php wp_footer(); ?>
+  <?php include_once("analyticstracking.php") ?>
   </body>
 </html>
--- page-contact.php
+++ page-contact.php
@@ -3,31 +3,22 @@
   
   get_header();
 
-  ?>
+  if (have_posts() ) :
+    while (have_posts() ) : the_post(); ?>
+
+    
+      <h2 class="section-header" id="my_bio">Contact</h2>
+      <p><?php the_content(); ?></p>
   
-  <section class="contact">
-    <h2 class="section-header" id="my_bio">My Bio</h2>
-    <div class="input__text--intro">
+      <?php    
 
-    <h2>If you wish to contact me, use the form below.</h2>
+      endwhile;
 
-    </div>
-    <form id="form" action="index.php#form" method="POST" novalidate="novalidate">
-      <div class="input__group"><label for="name">Name
-      <input id="name" name="name" type="text" /></label>
-      <label for="_replyto">Email
-      <input id="_replyto" name="_replyto" type="email" /></label></div>
-      <label for="textarea">Tell me about your project</label>
-      <textarea id="textarea" name="textarea"></textarea>
+    else : 
+      echo '<p>No content Found </p>';
 
-      <input type="submit" value="Send" />
+    endif;
 
-    </form>
-  </section>
-
-
-
-  <?php
 
     get_footer();
 
